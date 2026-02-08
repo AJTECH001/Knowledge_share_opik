24f4ca598f2e467a9c4139fdd7be31df58a70f26
AJTECH001 <ajtech255@gmail.com>
2026-02-08 21:31:43 +0100

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  serverExternalPackages: ["opik", "nunjucks", "chokidar"],
+  webpack: (config) => {
+    config.externals.push({
+      "fsevents": "commonjs fsevents"
+    });
+    return config;
+  }
+};
 module.exports = nextConfig;
